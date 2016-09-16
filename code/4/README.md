c8253e21318ceb9a870ad5c14a0f853f4eec26ac
Joe Fair <joe@fairanswers.com>
2016-09-16 17:20:51 -0400

    SA1

---
+++ code/4/README.md
@@ -0,0 +1,10 @@
++ASE16Homework Week 3
+
+[Simulated Annealer Code](./sa1_joef.py)
+
+![Simulated Annealer Output](https://raw.githubusercontent.com/fairanswers/fss16joe/master/code/4/_sa1.png)
+
+
+
+
+
--- code/4/sa1_joef.py
+++ code/4/sa1_joef.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 '''
-Modified from Dr. Menzies http://menzies.us/
+Modified template from Dr. Menzies http://menzies.us/
 '''
 from __future__ import division,print_function
-import sys,random,os
+import sys,random,os,math
 sys.dont_write_bytecode=True
+import pdb
 
 class Pretty(object):
   def __repr__(i):
@@ -26,10 +27,11 @@ def items(x):
 def say(x): 
   sys.stdout.write(str(x)); sys.stdout.flush()
 
-#Constants
+# Let's start at the very beginning...
 random.seed(1)
 
-
+smin = -10000 #Minimum solution
+smax =  10000 # Maximum solution
 
 def energy_schaffer(x):
   f1=x**2
@@ -37,47 +39,100 @@ def energy_schaffer(x):
   return f1, f2
 
 def baseline():
-  local_fmin=100000
-  local_fmax=0
-  f1=f2=0
+  global smin, smax
+  emin=sys.maxint
+  emax=-sys.maxint
+  basemin=0
+  basemax=0
   for x in range(100):
-    f1, f2 = energy_schaffer(x)
-    # check for min
-    if f1 < local_fmin:
-      local_fmin = f1
-    if f2 < local_fmin:
-      local_fmin = f2
-    # check for max
-    if f1> local_fmax:
-      local_fmax = f1
-    if f2> local_fmax:
-      local_fmax = f2
-  return local_fmin, local_fmax
+    s=random.randint(smin, smax)
+    e = s*s + (s-2)*(s-2)
+    if e < emin:
+      emin = e
+      basemin = s
+    if e> emax:
+      emax = e
+      basemax = s
+  print("Emin = %s=%s Emax = %s=%s" %(emin, basemin, emax, basemax) )
+  return emin, emax
 
-def schaffer_norm(f1, f2):
-  return ((f1+f2) - fmin) / (fmax - fmin)
+
+def P(e, en, kpct):
+  return kpct
+
+def neighbor(s):
+  "Hard coded to 10.  If out of bounds, bouce back"
+  jump=random.randint(-10, +10)
+  if s+jump < -10000 or s+jump > 10000:
+    return s-jump
+  return s+jump
+
+def E(s):
+  "Energy normalized from previous tests"
+  # From earlier baseline
+  emax = 198363364 
+  emin = 3874
+  f1=s*s
+  f2=(s-2)*(s-2)
+  return ((f1+f2) - emin) / (emax - emin)
+
 
 # Beginners tests
 def testFunction():
   #schaffer_norm1 = schaffer_norm(0,0)
   #print(schaffer_norm1)
   #print("EF = %s %s " % energy_schaffer(4) )
-  print("baseline fmin = %s max = %s" %baseline() )
+  baseline()
 
 def annealing():
-  #  These values come from experiments
-  fmax = 9801 # from baseline runs
-  fmin = 0 # from baseline runs
+  global smin, smax
   kmax = 1000 # number of attempts
-  emax = 10000 # maximum possible energy
+  k  = 0    # Current count
+  #  These values come from experiments
+  emax = 0 # maximum acceptable energy
 
-  s = 0 
-  e = sys.maxint
+  s = random.randint(smin,smax)    # Start Solution
+  e = E(s)
+  sn = 0
+  sb = 0    # Best Solution ( so far )
+  eb = .9
 
-  for k in xrange(kmax):
+  print("Starting annealing")
+  say(eb)
+  #say("asdf")
+  #say(emax)
+
+  while( k<kmax and e > emax ):
+    sn = neighbor(s) #  Get new neighbor
+    en = E(sn)       #  Get new neighbor's energy
+    #say ("solution sn=");say (sn );say (" en=");say (en );say ("\n");say (sb)
+    #pdb.set_trace()
+    #If new is better than BEST, make new BEST
+    if en < eb:
+      sb = sn
+      se = en
+      say("!")
+    #If new is better than CURRENT
+    if en < e: #Move there
+      s = sn
+      e = en
+      say("+")
+    else: 
+      #Do we jump?
+      if (P(e, en, k/kmax ) < random.random() ):
+        s = random.randint(smin, smax);
+        e = E(s)
+        say("?")
+      else:
+        say(".")
+    k += 1
+    if k%50==0:  say("\n"); say(eb)
+  print("Finised annealing.  Solution = %s Energy = %f" % (sb, eb) )
     
 
 
 if __name__ == '__main__':
-  if True:
+  if False:
     testFunction()
+  if True:
+    annealing()
