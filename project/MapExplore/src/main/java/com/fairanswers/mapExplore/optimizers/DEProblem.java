f6a9458ad64138c9b339176f0b180ed4e0107b9b
joe.fair <joe@bodkinconsulting.com>
2016-11-28 06:01:57 -0500

    Didn't check in DE and NSGAII Problems

---
+++ project/MapExplore/src/main/java/com/fairanswers/mapExplore/optimizers/DEProblem.java
@@ -0,0 +1,139 @@
+package com.fairanswers.mapExplore.optimizers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.InitialContext;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.singleobjective.differentialevolution.DifferentialEvolution;
+import org.uma.jmetal.algorithm.singleobjective.differentialevolution.DifferentialEvolutionBuilder;
+import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
+import org.uma.jmetal.operator.impl.selection.DifferentialEvolutionSelection;
+import org.uma.jmetal.problem.DoubleProblem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.evaluator.SolutionListEvaluator;
+import org.uma.jmetal.util.evaluator.impl.MultithreadedSolutionListEvaluator;
+import org.uma.jmetal.util.evaluator.impl.SequentialSolutionListEvaluator;
+
+import com.fairanswers.mapExplore.fsm.Model;
+
+public class DEProblem implements Runnable{
+
+	private int runs=1;
+	private int population;
+	private int evals;
+	private String startTime;
+	Runner runner;
+
+	@Override
+	public void run() {
+		DoubleProblem problem;
+	    //Algorithm<DoubleSolution> algorithm;
+	    DifferentialEvolutionSelection selection;
+	    DifferentialEvolutionCrossover crossover;
+	    SolutionListEvaluator<DoubleSolution> evaluator ;
+
+	    double cr = .3;
+	    double f = .9;
+	    String variant = "rand/1/bin";
+	    int seed = Model.getRandomIntRange(0, 10000);
+	    		
+	    problem = MapExploreProblem.create(seed);
+	    	    int numberOfCores =8;
+
+	    if (numberOfCores == 1) {
+	      evaluator = new SequentialSolutionListEvaluator<DoubleSolution>() ;
+	    } else {
+	      evaluator = new MultithreadedSolutionListEvaluator<DoubleSolution>(numberOfCores, problem) ;
+	    }
+
+	    crossover = new DifferentialEvolutionCrossover(cr,f,variant) ;
+	    selection = new DifferentialEvolutionSelection();
+
+	    DifferentialEvolution algorithm = new DifferentialEvolutionBuilder(problem)
+	        .setCrossover(crossover)
+	        .setSelection(selection)
+	        .setSolutionListEvaluator(evaluator)
+	        .setMaxEvaluations(evals )
+	        .setPopulationSize(population)
+	        .build() ;
+
+
+
+
+	    ArrayList<String> results = new ArrayList<>();
+		results.add("CoverageDE.cr="+cr+"_f="+f+"_variant="+variant+"_pop="+population+"_evals="+evals+"_seed"+seed );
+	    for(int i=0; i< runs; i++){
+		    List<DoubleSolution> initalPopulation = new ArrayList<>();
+			for(int j=0; j<population; j++){
+				DoubleSolution sol = problem.createSolution();
+				problem.evaluate(sol);
+				initalPopulation.add(sol);
+			}
+			
+		    AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+			        .execute() ;
+			List<DoubleSolution> fpop = algorithm.getPopulation() ;
+			for(DoubleSolution s : fpop){
+				results.add(Double.toString(s.getObjective(0) ) );
+				results.add(Double.toString(s.getObjective(1) ) );
+			}
+		    	
+		    String display = MapExploreProblem.generateParetofront(initalPopulation, fpop);
+			results.add("\nPARETO:\n"+display);
+
+	    }
+	    evaluator.shutdown();
+	    try {
+			runner.report("CoverageDE", startTime, results);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public int getRuns() {
+		return runs;
+	}
+
+	public void setRuns(int runs) {
+		this.runs = runs;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	public int getEvals() {
+		return evals;
+	}
+
+	public void setEvals(int evals) {
+		this.evals = evals;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public Runner getRunner() {
+		return runner;
+	}
+
+	public void setRunner(Runner runner) {
+		this.runner = runner;
+	}
+	
+
+}
---
+++ project/MapExplore/src/main/java/com/fairanswers/mapExplore/optimizers/NSGIIProblem.java
@@ -0,0 +1,141 @@
+package com.fairanswers.mapExplore.optimizers;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAII;
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.operator.impl.crossover.SBXCrossover;
+import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
+import org.uma.jmetal.operator.impl.selection.TournamentSelection;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.qualityindicator.impl.Hypervolume;
+import org.uma.jmetal.qualityindicator.impl.hypervolume.PISAHypervolume;
+import org.uma.jmetal.qualityindicator.impl.hypervolume.util.WfgHypervolumeFront;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.ProblemUtils;
+import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
+import org.uma.jmetal.util.front.Front;
+import org.uma.jmetal.util.front.util.FrontUtils;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
+import com.fairanswers.mapExplore.fsm.Model;
+
+public class NSGIIProblem implements Runnable{
+
+	private String startTime;
+	private int population;
+	private int evals;
+	private Runner runner;
+	private int runs=1;
+
+	@Override
+	public void run() {
+		Problem<DoubleSolution> problem;
+		NSGAII<DoubleSolution> algorithm;
+		CrossoverOperator<DoubleSolution> crossover;
+		MutationOperator<DoubleSolution> mutation;
+		SelectionOperator<List<DoubleSolution>, DoubleSolution> selection;
+		String referenceParetoFront = "";
+
+		String problemName = "com.fairanswers.mapExplore.optimizers.MapExploreProblem";
+		problem = ProblemUtils.<DoubleSolution>loadProblem(problemName );
+
+		int seed = Model.getRandomIntRange(0, 1000000);
+		double crossoverProbability = 0.9;
+		double crossoverDistributionIndex = 20.0;
+		crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
+
+		double mutationProbability = 1.0 / problem.getNumberOfVariables();
+		double mutationDistributionIndex = 20.0;
+		mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
+		
+		selection = new TournamentSelection<DoubleSolution>(
+				new RankingAndCrowdingDistanceComparator<DoubleSolution>(), 5);
+
+		algorithm = new NSGAIIBuilder<DoubleSolution>(problem, crossover, mutation).setSelectionOperator(selection)
+				.setMaxEvaluations(evals).setPopulationSize(population).build();
+
+		List<DoubleSolution> initalPopulation = new ArrayList<>();
+		for(int i=0; i<population; i++){
+			DoubleSolution sol = problem.createSolution();
+			problem.evaluate(sol);
+			initalPopulation.add(sol);
+		}
+		algorithm.setPopulation(initalPopulation);
+		ArrayList<String> results = new ArrayList<>();
+		String runName = "NSGII.crossoverProbability=" +crossoverProbability
+				+"_crossoverDistributionIndex="+crossoverDistributionIndex
+				+"_mutationProbability="+mutationProbability
+				+"_mutationDistributionIndex="+mutationDistributionIndex
+				+"_pop="+population+"_evals="+evals+"_seed"+seed;
+		results.add(runName );
+		for(int i=0; i< runs; i++){
+			System.out.println("Starting run "+i);
+			AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm).execute();
+			List<DoubleSolution> population = algorithm.getResult();
+			//System.out.println("Init ="+initalPopulation);
+			//System.out.println("Sols ="+population);
+			for(int j=0; j<population.size(); j++){
+				results.add(Double.toString(population.get(j).getObjective(0) ) );
+				results.add(Double.toString(population.get(j).getObjective(1) ) );
+			}
+			String display = MapExploreProblem.generateParetofront(initalPopulation, population);
+			results.add("\nPARETO:\n"+display);
+		}
+
+		//List<DoubleSolution> population = algorithm.getResult();
+	    try {
+			runner.report("NSGII", startTime, results);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JMetalLogger.logger.info("Random seed: " + JMetalRandom.getInstance().getSeed());
+
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	public int getEvals() {
+		return evals;
+	}
+
+	public void setEvals(int evals) {
+		this.evals = evals;
+	}
+
+	public Runner getRunner() {
+		return runner;
+	}
+
+	public void setRunner(Runner runner) {
+		this.runner = runner;
+	}
+
+	public void setRuns(int runs) {
+		this.runs=runs;
+	}
+}
